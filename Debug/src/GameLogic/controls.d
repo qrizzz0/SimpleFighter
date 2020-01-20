7680fec0da8160d5d0ff1b4d0dd60a84baf6dc74
qrizzz0 <47785023+qrizzz0@users.noreply.github.com>
2020-01-20 22:02:57 +0100

    Fuck debug mappen

--- Debug/src/GameLogic/controls.d
+++
@@ -1,9 +0,0 @@
-src/GameLogic/controls.o src/GameLogic/controls.o: \
- ../src/GameLogic/controls.c ../src/GameLogic/controls.h \
- ../src/GameLogic/character.h ../src/GameLogic/map.h
-
-../src/GameLogic/controls.h:
-
-../src/GameLogic/character.h:
-
-../src/GameLogic/map.h:
--- Debug/src/GameLogic/map.d
+++
@@ -1,4 +0,0 @@
-src/GameLogic/map.o src/GameLogic/map.o: ../src/GameLogic/map.c \
- ../src/GameLogic/map.h
-
-../src/GameLogic/map.h:
